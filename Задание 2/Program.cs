151f97302d2cbc6e12ca77f33673a047f6d27992
Марсель Мубаракшин <marselmubaraksin231@mail.ru>
2023-11-18 21:05:59 +0300

    Добавил

--- Задание 2/Program.cs
+++ Задание 2/Program.cs
@@ -1,9 +1,5 @@
 ﻿using System;
 
-public class MainClass
-{
-    public static void Main()
-    {
         Console.WriteLine("задано число N — количество элементов в массиве");
         int n = Convert.ToInt32(Console.ReadLine());
         if (n > 100)
@@ -13,37 +9,33 @@ public class MainClass
         }
 
 
-        Console.WriteLine("Далее запишите N чисел — элементы массива через пробел.");
-        string[] h = Console.ReadLine().Split(' ');
-        if (h.Length != n)
-        {
-            Console.WriteLine("Количество чисел не соответствует заданному количеству элементов в массиве");
-            return;
-        }
-        Console.WriteLine("Ответ:");
+        Console.WriteLine("Далее запишите N чисел.");
         int[] k = new int[n];
-        int sum = 0;
-
-
 
         for (int i = 0; i < n; i++)
         {
-            if (!int.TryParse(h[i], out k[i]) || Math.Abs(k[i]) > 100)
+            string input = Console.ReadLine();
+
+            if (!int.TryParse(input, out k[i]) || Math.Abs(k[i]) > 100)
             {
                 Console.WriteLine("Элементы массива должны быть целыми числами, каждое из которых по модулю не превосходит 100");
                 return;
             }
-            k[i] = Convert.ToInt32(h[i]);
-            int j = 0;
-            for (; j < i; j++)
-                if (k[i] + k[j] == 0)
-                    break;
-            if (i > j)
+        }
+
+
+
+        Console.WriteLine("Ответ:");
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < i; j++)
             {
-                Console.Write($"{j} {i}");
-                break;
+                if (k[i] + k[j] == 0)
+                {
+                    Console.WriteLine($"{j} {i}");
+                    return;
+                }
             }
         }
+
         Console.ReadKey();
-        }
-}
--- Задание 3/Program.cs
+++ Задание 3/Program.cs
@@ -1,29 +1,29 @@
 ﻿using System;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        int n = Convert.ToInt32(Console.ReadLine()); // количество чисел в таблице
-        int[] temperatures = new int[n];
+        Console.WriteLine("задайте количество дней когда вы измерели температуру ");
+        int n = int.Parse(Console.ReadLine());
 
-        // Считываем значения температуры из входных данных
+        int[] temperatures = new int[n];
+        Console.WriteLine("Укажите значения температуры  ");
+
         for (int i = 0; i < n; i++)
         {
-            temperatures[i] = Convert.ToInt32(Console.ReadLine());
+            temperatures[i] = int.Parse(Console.ReadLine());
         }
 
-        // Перебираем значения температуры и ищем локальные минимумы и максимумы
-        for (int i = 1; i < n - 1; i++)
-        {
-            if (temperatures[i] < temperatures[i - 1] && temperatures[i] < temperatures[i + 1])
-            {
-                Console.WriteLine("day {0}: {1} (min)", i + 1, temperatures[i]);
-            }
-            else if (temperatures[i] > temperatures[i - 1] && temperatures[i] > temperatures[i + 1])
-            {
-                Console.WriteLine("day {0}: {1} (max)", i + 1, temperatures[i]);
-            }
-        }
+
+Console.WriteLine("Вывод:");
+// Перебираем значения температуры и ищем локальные минимумы и максимумы
+for (int i = 1; i < n - 1; i++)
+{
+    if (temperatures[i] < temperatures[i - 1] && temperatures[i] < temperatures[i + 1])
+    {
+        Console.WriteLine("day {0}: {1} (min)", i + 1, temperatures[i]);
+    }
+    else if (temperatures[i] > temperatures[i - 1] && temperatures[i] > temperatures[i + 1])
+    {
+        Console.WriteLine("day {0}: {1} (max)", i + 1, temperatures[i]);
     }
 }
+    
+
